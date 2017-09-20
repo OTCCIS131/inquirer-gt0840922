f7b6ca9e7796d61cea1727ff6d2063baddfa8eb0
Garrett Troy <gt0840922@otc.edu>
2017-09-20 13:25:53 -0500

    Updates

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ This is what you'll expect to do in the program:
 
     A user executes the program
     They are greeted with a friendly message from "My Super Awesome Pizza Co."
-    They are asked which size pizza they'd like
-    They are asked which toppings they'd like
-    They are asked which sauce they want
-    Their order is repeated back to them with pretty colors
-    The program exits
+    They are asked which size pizza they'd like.
+    They are asked which toppings they'd like.
+    They are asked which sauce they want.
+    Their order is repeated back to them with pretty colors.
+    The program exits.
 
 The End!
--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 var inquirer = require('inquirer');
+console.log ("Welcome to Petey's Pizza Palace of Pizzaz.")
+console.log ("Our phone number is 1-888-555-9222")
+
 let questions = [{
     name: "Size",
     message: "What size is the Pizza?",
+    type: "list",
     choices: ["Small", "Medium", "Large", "Extra Large"],
 },{
     name: "Toppings",
     message: "What kind of toppings do you want for your Pizza?",
-    choices: ["Pepperoni", "Sausage", "No toppings", "Caanadian Bacon", "Supreme"],
+    type: "checkbox",
+    choices: ["Pepperoni", "Sausage", "Canadian Bacon", "Supreme", "N/A (Cheese)"],
 },{
     name: "Sauce",
-    message: "What kind of sauce do you want for your Pizza?",
-    choices: ["Marinara", "Cheese"],
+    message: "What kind of dipping sauce do you want for your Pizza?",
+    type: "list",
+    choices: ["Marinara", "Cheese", "N/A", "Horse Radish", "Chili", "Honey"],
 }]
+
 inquirer.prompt(questions).then(function (answers) {
-    console.log (answers)
+    console.log ("You ordered a/n " + answers.Size + " Pizza.")
+    console.log ("Your Pizza topping/s is/are " + answers.Toppings + ".")
+    console.log ("You wanted " + answers.Sauce + " dipping sauce for your Pizza.")
+
+    let correct = [{
+        name: "Correct",
+        message: "Is this Correct?",
+        type: "confirm",
+    }]
+
+    inquirer.prompt(correct).then(function (answers) {        
+        if (answers.Correct == true){
+            console.log (answers.Correct + "? Well, that's good.")
+        }
+        
+        else{
+            console.log (answers.Correct + "? Oh, we apologize for that.")
+        }
+        
+    });
 });
